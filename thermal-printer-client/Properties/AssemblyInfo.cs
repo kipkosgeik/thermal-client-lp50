52c55b46be881c15e167c4e1c2d0c381414d8146
Kenny <kenny.kosgei@micromobile.co.ke>
2017-06-08 19:13:16 +0300

    Initial commit

--- thermal-printer-client/Properties/AssemblyInfo.cs
+++ thermal-printer-client/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("thermal-printer-client")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Einsroot Limited")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
