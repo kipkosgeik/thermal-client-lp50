a52c3607d7ab4b326592b2979105fa9da1671e73
Kenny <kenny.kosgei@micromobile.co.ke>
2017-06-08 19:14:28 +0300

    Add installshield setup wizard

--- thermal-printer-client/SerialPortTimer.cs
+++ thermal-printer-client/SerialPortTimer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Timers;
-using System.Threading.Tasks;
+﻿using System.Timers;
 
 
 namespace thermal_printer_client
--- thermal-printer-client/ThermalPrinterBridge.cs
+++ thermal-printer-client/ThermalPrinterBridge.cs
@@ -4,9 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace thermal_printer_client
 {
@@ -19,46 +17,60 @@ namespace thermal_printer_client
     /// 
     /// It requires command line arg file_name which should be a CSV with format:
     /// <code>FORM_NAME,Variable_count,V00,V01,..,Vnn</code>
-    /// <para></para>
+    /// <para>Note that graphics are supposed to be preloaded in the printer. The DATECS label editor is best for this task.</para>
     /// </summary>
     class ThermalPrinterBridge
     {
         private const int PORT_READ_TIMEOUT = 10000;
         private const int PORT_WRITE_TIMEOUT = 10000;
-        private string TARGET_FORM = "L5";
 
         static void Main(string[] args)
         {
-            try {
-                //temporary
-                //args = new string[] { "My file" };
-                if (args.Length == 0) {
+            try
+            {
+
+                if (args.Length == 0)
+                {
                     throw new Exception("Usage: Please include file name as an argument to this program.");
-                } else {
-                    Console.WriteLine(string.Format("Reading file {0}...", args[0]));
-                    ThermalPrinterBridge bridge = new ThermalPrinterBridge(); 
+                }
+                else
+                {
+
+                    ThermalPrinterBridge bridge = new ThermalPrinterBridge();
+                    Console.WriteLine("Reading file {0}...", args[0]);
                     List<string> variables = bridge.ReadValues(args[0]);
-                    const string LP_50_PORT = "COM3"; //fetch port from config
+                    Console.WriteLine("Acquired data: {0}", string.Join(" ", variables.ToArray()));
+                    string LP_50_PORT = Properties.Settings.Default.serialPort; //fetch port from config
+                    Console.WriteLine("Configured port is {0}", LP_50_PORT);
                     List<String> ports = bridge.getAllPorts();
                     Console.WriteLine("Enumerating available ports...");
                     bool targetPortFound = false;
-                    foreach (string port in ports) {
-                        if (port.ToLower().Equals(LP_50_PORT.ToLower())) { //select the configured port
+                    foreach (string port in ports)
+                    {
+                        if (port.ToLower().Equals(LP_50_PORT.ToLower()))
+                        { //select the configured port
                             targetPortFound = true;
                             SerialPort serialPort = null;
-                            try {
+                            try
+                            {
                                 serialPort = new SerialPort(port);
-                                if (serialPort.IsOpen == false) { //if not open, open the port
+                                if (serialPort.IsOpen == false)
+                                { //if not open, open the port
                                     serialPort.Open();
                                     serialPort.ReadTimeout = PORT_READ_TIMEOUT;
                                     serialPort.WriteTimeout = PORT_WRITE_TIMEOUT;
-                                    Console.WriteLine("Communicating through " + port);
-                                    bridge.ConverseWithPrinter(serialPort, variables[0], variables.GetRange(2, variables.Count() - 1));
-                                } else {
+                                    Console.WriteLine("Communicating through {0}", port);
+                                    bridge.ConverseWithPrinter(serialPort, variables[0], variables.GetRange(2, int.Parse(variables[1])));
+                                }
+                                else
+                                {
                                     throw new Exception(string.Format("Serial port {0} is already in use by another program.", LP_50_PORT));
                                 }
-                            } finally {
-                                if (serialPort != null) { //always close port unconditionally
+                            }
+                            finally
+                            {
+                                if (serialPort != null)
+                                { //always close port unconditionally
                                     Console.WriteLine("Closing port..");
                                     serialPort.Close();
                                 }
@@ -69,15 +81,20 @@ namespace thermal_printer_client
                         }
                     }
 
-                    if (targetPortFound) {
-                        Console.WriteLine("\n\nENTER to exit");
+                    if (targetPortFound)
+                    {
+                        Console.WriteLine("\n\npress ENTER to exit");
                         Console.ReadLine(); //wait for user to press any button -- Temporary
-                    } else {
+                    }
+                    else
+                    {
                         throw new Exception(String.Format("Printer not found on port {0}. Please connect printer on correct USB port.", LP_50_PORT));
                     }
                 }
-            } catch (Exception e) {
-                Console.Error.WriteLine(string.Format("{0}\n\nENTER to exit", e.Message));
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("{0}\n\nENTER to exit", e.Message);
                 Console.ReadLine(); //wait for user to press any button
             }
 
@@ -92,23 +109,32 @@ namespace thermal_printer_client
         /// <exception cref="Exception">IO errors and other malfunctions will be wrapped under generic exception</exception>
         public List<string> ReadValues(string filePath)
         {
-            StreamReader streamReader = null;
-            try {
+            TextReader streamReader = null;
+            try
+            {
                 List<string> values = new List<string>();
 
                 FileStream f = new FileStream(filePath, FileMode.Open);
                 streamReader = new StreamReader(f);
+
                 CsvConfiguration config = new CsvConfiguration();
                 config.Delimiter = ",";
+                config.HasHeaderRecord = false;
+                config.TrimFields = true;
                 CsvReader csvReader = new CsvReader(streamReader, config);
                 while (csvReader.Read())
-                    values.Add(csvReader.GetRecord<string>());
+                    values.AddRange(new List<String>(csvReader.CurrentRecord));
 
                 return values;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 throw new Exception("Error reading file. " + e.Message);
-            } finally {
-                if (streamReader != null) {
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
                     streamReader.Close();
                 }
             }
@@ -121,7 +147,8 @@ namespace thermal_printer_client
         public List<string> getAllPorts()
         {
             List<String> allPorts = new List<String>();
-            foreach (String portName in System.IO.Ports.SerialPort.GetPortNames()) {
+            foreach (String portName in System.IO.Ports.SerialPort.GetPortNames())
+            {
                 allPorts.Add(portName);
             }
             return allPorts;
@@ -137,47 +164,54 @@ namespace thermal_printer_client
         /// <param name="variables">the values to substitute in the form variables, they SHOULD be exact NUMBER as in form for best results. The variables SHOULD ALSO be IN THE ORDER they are declared in the forms e.g from V00,V01...Vnn</param>
         public void ConverseWithPrinter(SerialPort port, string targetForm, List<String> variables)
         {
-            try {
+            try
+            {
 
                 //Read installed forms in printer memory and confirm if required form is available
                 //Us UF command to list forms
                 port.WriteLine("UF");
-                List<String> forms = ReadPortResponse(port, PORT_READ_TIMEOUT, true);
+                List<String> forms = ReadPortResponse(port, PORT_READ_TIMEOUT * 2, true);
                 //forms are listed starting with the form count as first item e.g
                 //002
                 //L0
                 //L1
                 //means there are 2 forms named L0,L1
                 bool targetFormAvailable = false;
-                foreach (string form in forms) {
+                foreach (string form in forms)
+                {
                     Console.WriteLine(form);
-                    if (form.ToLower().Equals(targetForm.ToLower())) { //Form names are case insensitive
+                    if (form.ToLower().Equals(targetForm.ToLower()))
+                    { //Form names are case insensitive
                         targetFormAvailable = true;
                     }
                 }
 
-                if (targetFormAvailable) {
-                    //Load & activate target form, user FR command
-                    port.WriteLine(string.Format("FR\"{0}\"", targetForm));
-                    //read all form prompts and respond till no more prompts, then print label
-                    //It is very important that the variable be equal number with the prompts, otherwise
-                    //printer will remain in prompt mode and won't take other commands with expected behavior
-                    port.WriteLine("?"); //prompt printer to start variable & counter prompts.
-
-                    //Write out variables to substitute each prompt.
-                    foreach (string variable in variables) {
-                        port.WriteLine(variable);
-                    }
-                    //flush out any bytes
-                    port.BaseStream.Flush();
-
-                    //Print one label out of the above variables. Uses athe P command.
-                    port.WriteLine("P1,1");
-                    Console.WriteLine("Completed");
-                } else {
-                    Console.Error.WriteLine(string.Format("Form {0} not found in printer memory, please try again. Contact system admin if problem persists.", TARGET_FORM));
+                if (!targetFormAvailable)
+                {
+                    Console.WriteLine("WARNING Form {0} not found in printer memory, printer may not be properly configured.", targetForm);
                 }
-            } finally {
+
+                //Load & activate target form, user FR command
+                port.WriteLine(string.Format("FR\"{0}\"", targetForm));
+                //read all form prompts and respond till no more prompts, then print label
+                //It is very important that the variable be equal number with the prompts, otherwise
+                //printer will remain in prompt mode and won't take other commands with expected behavior
+                port.WriteLine("?"); //prompt printer to start variable & counter prompts.
+
+                //Write out variables to substitute each prompt.
+                foreach (string variable in variables)
+                {
+                    port.WriteLine(variable);
+                }
+                //flush out any bytes
+                port.BaseStream.Flush();
+
+                //Print one label out of the above variables. Uses athe P command.
+                port.WriteLine("P1,1");
+                Console.WriteLine("Completed");
+            }
+            finally
+            {
                 port.BaseStream.Flush();
                 port.Close();
             }
@@ -203,30 +237,43 @@ namespace thermal_printer_client
 
             List<String> response = new List<String>();
 
-            if (port.BytesToRead > 0) {
-                try {
+            if (port.BytesToRead > 0)
+            {
+                try
+                {
                     int c;
                     StringBuilder b = new StringBuilder();
                     //read line by line
-                    while ((c = port.ReadByte()) != '\n' || port.BytesToRead > 0) {
-                        if (c == '\r') { //a line of input has been read
+                    while ((c = port.ReadByte()) != '\n' || port.BytesToRead > 0)
+                    {
+                        if (c == '\r')
+                        { //a line of input has been read
                             response.Add(b.ToString());
                             b.Clear();
                             //if multiple lines are expected, we wait for a while for next bytes to be written to buffer if any
-                            if (multiple) {
+                            if (multiple)
+                            {
                                 timer.Start(PORT_READ_TIMEOUT);
                                 while (port.BytesToRead == 0 && !timer.timedout) ;
                             }
-                        } else if (c == '\n') {
+                        }
+                        else if (c == '\n')
+                        {
                             continue; //Continue to new line
-                        } else {
+                        }
+                        else
+                        {
                             b.Append((char)c);
                         }
                     }
-                } catch (TimeoutException e) {
-                    Console.Error.WriteLine(string.Format("Read timed out. {0}", e.Message));
                 }
-            } else {
+                catch (TimeoutException e)
+                {
+                    Console.Error.WriteLine("Read timed out. {0}", e.Message);
+                }
+            }
+            else
+            {
                 Console.WriteLine("Read timed out");
             }
 
